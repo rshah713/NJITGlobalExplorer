7b00614dd401e6c3465a2f2919224543e71a0b64
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2024-06-08 00:27:07 -0400

    initial animation!

--- app/static/js/animations.js
+++ app/static/js/animations.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.classList.add('body-animate');
+// animations.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    const mainContent = document.getElementById('main-content');
+
+    // Add a click event listener to the loading screen
+    loadingScreen.addEventListener('click', function() {
+        // Hide the loading screen
+        loadingScreen.style.display = 'none';
+
+        // Show the main content
+        mainContent.style.display = 'block';
+    });
 });
