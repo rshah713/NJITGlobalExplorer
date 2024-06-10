6fdeaf2e3e42c50604a470a6f5660d6c8074d2f1
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2024-06-10 15:33:16 -0400

    begin OAuth integration

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-import ssl
-import os 
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-current_directory = os.path.dirname(os.path.abspath(__file__))
-file_path1 = os.path.join(current_directory, 'server.crt')
-file_path2 = os.path.join(current_directory, 'server.key')
-context.load_cert_chain(file_path1, file_path2)
 
 @app.route('/')
 def index():
@@ -37,7 +30,7 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context=context)
+    app.run(debug=True)
     # secret passphrase: 'secret'
     # https://kracekumar.com/post/54437887454/ssl-for-flask-local-development/ (but use 2048-bit keys)
 
