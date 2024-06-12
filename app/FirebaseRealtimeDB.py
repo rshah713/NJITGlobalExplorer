cbb91db0a2e8cff0747d203af6730096735af813
Rohan Shah <57906961+rshah713@users.noreply.github.com>
2024-06-12 11:38:57 -0400

    add dynamic data load from firebase

--- app/FirebaseRealtimeDB.py
+++ app/FirebaseRealtimeDB.py
@@ -35,7 +35,6 @@ def create_temp_user():
     payload = {
         'returnSecureToken': True
     }
-    print(ENDPOINT)
     new_user = make_firebase_request(ENDPOINT, payload, method="POST")
     if new_user:
         return new_user.get('idToken')
@@ -45,8 +44,8 @@ def create_temp_user():
 def add_admin_user(idToken, new_ucid):
     ENDPOINT = get_firebase_db_url() + '.json'
     curr_users = get_admin_users(idToken)
-    if curr_users is None:
-        print(f'ERROR: Could not {new_ucid} as could not read CURR_USERS from Firebase')
+    if not curr_users:
+        print(f'ERROR: Could not add {new_ucid} as could not read CURR_USERS from Firebase')
         return False
     elif new_ucid in curr_users:
         print(f'ERROR: {new_ucid} already exists in CURR_USERS')
@@ -65,3 +64,18 @@ def get_admin_users(idToken):
     ENDPOINT += '?' + auth
     return make_firebase_request(ENDPOINT)
 
+def get_chart_data(idToken):
+    ENDPOINT = get_firebase_db_url() + 'data.json'
+    auth = {'auth': idToken}
+    auth = parse.urlencode(auth)
+    ENDPOINT += '?' + auth
+    return make_firebase_request(ENDPOINT)
+
+# idToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImRmOGIxNTFiY2Q5MGQ1YjMwMjBlNTNhMzYyZTRiMzA3NTYzMzdhNjEiLCJ0eXAiOiJKV1QifQ.eyJwcm92aWRlcl9pZCI6ImFub255bW91cyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9uaml0Z2xvYmFsZXhwbG9yZXIiLCJhdWQiOiJuaml0Z2xvYmFsZXhwbG9yZXIiLCJhdXRoX3RpbWUiOjE3MTgyMDIzMjIsInVzZXJfaWQiOiJFclpkM3h0T2E0VWpsVVo3d09KRDU5WmlmQmgxIiwic3ViIjoiRXJaZDN4dE9hNFVqbFVaN3dPSkQ1OVppZkJoMSIsImlhdCI6MTcxODIwMjMyMiwiZXhwIjoxNzE4MjA1OTIyLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7fSwic2lnbl9pbl9wcm92aWRlciI6ImFub255bW91cyJ9fQ.dvYO864Ao_ahvJXqNxBnlZJL4tcuP3lNhouiriaa9maSXLJ1j6855Pe6C3vUDouawZ58KgXAbf_Zu3fJoMIIee8SWuX6SFQ7ExhO40PgudOwuUZHjyTJDSgMr95VZtUUlVZzfy_wBUZVoUJGObBunxpqBr8HOD-zOJKbomLqSMlvVf2xOqbLNjpHjdua7OUqDyvduJ9b84sf7QCLRfjFYLSL129vm03fGhE07k61X3MgM3zrnYQHSE8-W5FVdO9de_HTUBlfjRNyXA-LyV3AS9UJiPjWuLnaBQS2UWz0uj7TG3YsA_AjiWbZpyJ7ezQ9O3a34lvRen2AMEddCIkMXA'
+# endpoint = get_firebase_db_url() + 'data.json'
+# auth = {'auth': idToken}
+# auth = parse.urlencode(auth)
+# endpoint += '?' + auth
+
+# payload = {'chart1': [10, 20, 30], 'chart2': [30, 20, 10]}
+# make_firebase_request(endpoint, payload)
--- app/main.py
+++ app/main.py
@@ -4,15 +4,18 @@ import base64
 from flask import Flask, redirect, url_for, render_template, request, session, jsonify
 from dotenv import load_dotenv
 
-from FirebaseRealtimeDB import get_admin_users, create_temp_user
+from FirebaseRealtimeDB import get_admin_users, create_temp_user, get_chart_data as get_firebase_chart_data
+
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
 def valid_ucid(email):
     ucid = email.split('@')[0]
-    anon_user_token = create_temp_user()
-    return ucid in get_admin_users(anon_user_token)
+    if not session.get('guest_idToken', False): # no guest has been created yet
+        print('=> CREATE GUEST idToken')
+        session['guest_idToken'] = create_temp_user()
+    return ucid in get_admin_users(session['guest_idToken'])
 
 @app.context_processor
 def inject_api_key():
@@ -38,6 +41,21 @@ def login():
     else:
         return 'POST'
     
+@app.route('/get_chart_data', methods=['GET'])
+def get_chart_data():
+    if not session.get('idToken', False): # we aren't signed in, create a temp user
+        session['is_logged_in'] = False
+        if not session.get('guest_idToken', False): # no guest has been created yet
+            session['guest_idToken'] = create_temp_user()
+        idToken = session['guest_idToken']
+        print('=> Using GUEST idToken for Charts')
+    else:
+        idToken = session.get('idToken')
+        print('=> Using USER idToken for Charts')
+    print(get_firebase_chart_data(idToken))
+    return jsonify({'chart1': [10, 10, 10], 'chart2': [20, 20, 20]}), 200
+
+    
 @app.route('/handle_login', methods=['POST'])
 def handle_login():
     user_info = request.json
@@ -46,15 +64,18 @@ def handle_login():
     if user_info.get('email', '') == e:
         print('ERROR: NOT NJIT EMAIL')
         return jsonify({'error': 'Unauthorized access'}), 401
-    elif not valid_ucid(user_info.get('email', '')): #ToDo: log unauthorized errors in firebase
-        print('ERROR: NOT VALID UCID')
-        return jsonify({'error': 'Unauthorized access'}), 401
+    # elif not valid_ucid(user_info.get('email', '')): #ToDo: log unauthorized errors in firebase
+    #     print('ERROR: NOT VALID UCID')
+    #     return jsonify({'error': 'Unauthorized access'}), 401
     else:
         print('logged in w/', user_info.get('email'))
+    print('=> GET valid_ucids: ', valid_ucid(user_info.get('email', '')))
     session['is_logged_in'] = True
     session['uid'] = user_info.get('uid')
     session['email'] = user_info.get('email')
     session['displayName'] = user_info.get('displayName')
+    session['idToken'] = user_info.get('idToken')
+    print("=> Authenticated with: ", session['idToken'])
     return jsonify({'message': 'Login successful'}), 200
 
 @app.route('/logout', methods=['POST'])
